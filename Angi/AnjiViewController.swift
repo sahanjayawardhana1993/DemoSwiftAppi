1e9352481e9aaadf8501f6a7bb48e84cd0c369fc
Digital-05 <>
2019-03-07 20:25:33 +0530

    my first co

---
+++ Angi/AnjiViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AnjiViewController.swift
+//  Angi
+//
+//  Created by Digital-05 on 3/7/19.
+//  Copyright © 2019 Digital-05. All rights reserved.
+//
+
+import UIKit
+
+class AnjiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AngiTests/AngiTests.swift
@@ -0,0 +1,34 @@
+//
+//  AngiTests.swift
+//  AngiTests
+//
+//  Created by Digital-05 on 3/7/19.
+//  Copyright © 2019 Digital-05. All rights reserved.
+//
+
+import XCTest
+@testable import Angi
+
+class AngiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
